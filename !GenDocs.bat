168cef9773773ebe636e6b6aa79b1d80b36b6ecd
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2006-03-28 21:23:38 +0000

    Fixed up doxygen config

--- !GenDocs.bat
+++ !GenDocs.bat
@@ -1,3 +1,4 @@
-del /Q html\*.*
-"d:\Program Files\doxygen\bin\doxygen.exe" DOxygenStuff/doxygen.cfg 2> ../doxygen_output.txt
+rmdir /S /Q html
+mkdir html
+"g:\Program Files\doxygen\bin\doxygen.exe" DOxygenStuff/doxygen.cfg 2> doxygen_output.txt
 copy DOxygenStuff\tornado_wm.png html
