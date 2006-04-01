4a840726f96cd53afe13285f651055fa60cb7dec
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2006-04-01 14:14:56 +0000

    Fixed last remaining problems with docs

--- !GenDocs.bat
+++ !GenDocs.bat
@@ -1,4 +1,4 @@
 rmdir /S /Q html
 mkdir html
-"g:\Program Files\doxygen\bin\doxygen.exe" DOxygenStuff/doxygen.cfg 2> doxygen_output.txt
 copy DOxygenStuff\tornado_wm.png html
+"g:\Program Files\doxygen\bin\doxygen.exe" DOxygenStuff/doxygen.cfg 2> doxygen_output.txt
