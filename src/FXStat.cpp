83b109b6ba79f663951edf0d010384268da4789e
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2006-05-16 16:53:42 +0000

    v1.6 only fixes to get v1.6 based build working on Apple Mac OS X

--- src/FXStat.cpp
+++ src/FXStat.cpp
@@ -83,6 +83,8 @@ bool FXStat::readMetadata(const FXString &path, FXuint *flags, FXfval *size, FXT
 			{
 #if defined(__linux__)
 				// Unsupported at present
+#elif defined(__APPLE__)
+				// Unsupported at present
 #elif defined(__FreeBSD__)
 				created->set_time_t(st.st_birthtimespec.tv_sec);
 				created->value+=st.st_birthtimespec.tv_nsec/1000;
@@ -95,7 +97,7 @@ bool FXStat::readMetadata(const FXString &path, FXuint *flags, FXfval *size, FXT
 #if defined(__linux__)
 				lastModified->set_time_t(st.st_mtim.tv_sec);
 				lastModified->value+=st.st_mtim.tv_nsec/1000;
-#elif defined(__FreeBSD__)
+#elif defined(__APPLE__) || defined(__FreeBSD__)
 				lastModified->set_time_t(st.st_mtimespec.tv_sec);
 				lastModified->value+=st.st_mtimespec.tv_nsec/1000;
 #else
@@ -107,7 +109,7 @@ bool FXStat::readMetadata(const FXString &path, FXuint *flags, FXfval *size, FXT
 #if defined(__linux__)
 				lastAccessed->set_time_t(st.st_atim.tv_sec);
 				lastAccessed->value+=st.st_atim.tv_nsec/1000;
-#elif defined(__FreeBSD__)
+#elif defined(__APPLE__) || defined(__FreeBSD__)
 				lastAccessed->set_time_t(st.st_atimespec.tv_sec);
 				lastAccessed->value+=st.st_atimespec.tv_nsec/1000;
 #else
@@ -194,7 +196,7 @@ void FXStat::writeMetadata(const FXString &path, const FXTime *created, const FX
 			times[0].tv_usec=orig.st_atim.tv_nsec/1000;
 			times[1].tv_sec =orig.st_mtim.tv_sec;
 			times[1].tv_usec=orig.st_mtim.tv_nsec/1000;
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__APPLE__)
 			times[0].tv_sec =orig.st_atimespec.tv_sec;
 			times[0].tv_usec=orig.st_atimespec.tv_nsec/1000;
 			times[1].tv_sec =orig.st_mtimespec.tv_sec;
