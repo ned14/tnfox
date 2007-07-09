91dfb9d92f4ee2883906bb09c6795407741117af
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2007-07-09 15:15:09 +0000

    Stopped unhandled signal handler being recursed into

--- src/FXProcess.cxx
+++ src/FXProcess.cxx
@@ -495,9 +495,9 @@ static LONG MyUnhandledExceptionFilter(EXCEPTION_POINTERS *ei)
 }
 #endif
 #ifdef USE_POSIX
+static int MyUnhandledSignalHandlerCount;
 static void MyUnhandledSignalHandler(int sig, siginfo_t *si, void *data)
 {
-	RunFatalExitCalls(true);
 	FXString codestr("Unknown");
 	switch(sig)
 	{
@@ -633,6 +633,8 @@ static void MyUnhandledSignalHandler(int sig, siginfo_t *si, void *data)
 	fprintf(stderr, "\nUnhandled Signal 0x%x (%s)\n    at address %p - immediate exit!\n",
 		sig, codestr.text(), si->si_addr);
 	fflush(stderr);
+	if(!MyUnhandledSignalHandlerCount++)
+		RunFatalExitCalls(true);
 	_exit(EXIT_FAILURE);
 }
 #endif
