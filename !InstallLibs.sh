87a3a3004dab987220f8d54f0b55a4f3496b20f3
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2010-12-09 01:01:44 +0000

    Added a shell script to install dev libraries for you on Ubuntu/Debian.

---
+++ !InstallLibs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Installing libraries used for building TnFOX on Ubuntu/Debian ..."
+sudo apt-get install build-essential libtool g++ git-core git-gui libx11-dev libxext-dev libxcursor-dev libxft-dev libcups2-dev libpam0g-dev zlib1g-dev libtiff4-dev libpng12-dev libjpeg62-dev libbz2-dev libssl-dev libgl1-mesa-dev libglu1-mesa-dev
